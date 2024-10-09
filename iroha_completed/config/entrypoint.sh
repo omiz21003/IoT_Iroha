0e2cada153e897b46ade31edd2155d20916e4046
omiz21003 <105770936+omiz21003@users.noreply.github.com>
2024-10-09 16:33:25 +0900

    Update entrypoint.sh

--- iroha_completed/config/entrypoint.sh
+++ iroha_completed/config/entrypoint.sh
@@ -12,7 +12,7 @@ IROHA_NODEKEY=${IROHA_NODEKEY:-testnode2}
 echo "$ Can you see me? irohad --config ${IROHA_CONF} --genesis_block ${IROHA_GENESIS} --keypair_name ${IROHA_NODEKEY} --drop_state"
 
 irohad --config ${IROHA_CONF} \
+  --overwrite_ledger \ #これを消せばコンテナを消しても記録が残り続ける
   --genesis_block ${IROHA_GENESIS} \
   --keypair_name ${IROHA_NODEKEY} \
-  --drop_state \
-  --overwrite_ledger
+  --drop_state 
